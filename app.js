0d740550bded4cb28d94b763ae0c2bc51327b669
ktlaguardia <97317434+ktlaguardia@users.noreply.github.com>
2023-03-21 21:30:23 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
 "use strict";
 
 console.log("hello");
+
+const navBar = document.querySelector('.nav-bar');
+const navBtns = navBar.children;
+/*--------buttons hover animation------*/
+const buttonsHoverInit = function(){
+    for (let i = 0; i<navBtns.length;i++){
+        const button = navBtns[i];
+        if(button.outerHTML.startsWith('<li>')){
+            const child = navBtns[i].firstChild;
+            child.style.transition = '.25s';
+            button.style.transition = '.25s';
+            navBtns[i].addEventListener('mouseover', function(){
+                button.style.backgroundColor = 'white';
+                child.style.backgroundColor = 'white';
+                button.style.color = 'black';
+                child.style.color = 'black';
+                
+            });
+            navBtns[i].addEventListener('mouseout', function(){
+                button.style.backgroundColor = 'grey';
+                child.style.backgroundColor = 'grey';
+                button.style.color = 'white';
+                child.style.color = 'white';
+        });
+    }
+    }
+};
+buttonsHoverInit();
