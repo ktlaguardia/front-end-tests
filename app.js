a19ae8bccd6d420a99dbb97c82a4d4a9973e3436
ktlaguardia <97317434+ktlaguardia@users.noreply.github.com>
2023-03-26 19:22:35 +0000

    updated homepage: needs responsive

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 "use strict";
+AOS.init();
 
 console.log("goodbye");
 
@@ -8,17 +9,17 @@ const navBtns = navBar.children;
 const buttonsHoverInit = function () {
   for (let i = 0; i < navBtns.length; i++) {
     const button = navBtns[i];
-    if (button.outerHTML.startsWith("<li>")) {
+    if (button.outerHTML.startsWith("<li")) {
       const child = navBtns[i].firstChild;
       navBtns[i].addEventListener("mouseover", function () {
-        button.style.backgroundColor = "white";
-        child.style.backgroundColor = "white";
+        button.style.backgroundImage = "linear-gradient(rgb(121, 100, 71), rgb(121, 100, 71), rgb(121, 100, 71), rgb(109, 139, 113))";
+        child.style.backgroundImage = "linear-gradient(rgb(121, 100, 71), rgb(121, 100, 71), rgb(121, 100, 71), rgb(121, 100, 71), rgb(109, 139, 113))";
         button.style.color = "black";
         child.style.color = "black";
       });
       navBtns[i].addEventListener("mouseout", function () {
-        button.style.backgroundColor = 'rgb(94, 81, 81)';
-        child.style.backgroundColor = 'rgb(94, 81, 81)';
+        button.style.backgroundImage = 'linear-gradient(rgb(121, 100, 71), rgb(121, 100, 71))';
+        child.style.backgroundImage = 'linear-gradient(rgb(121, 100, 71), rgb(121, 100, 71))';
         button.style.color = "white";
         child.style.color = "white";
       });
