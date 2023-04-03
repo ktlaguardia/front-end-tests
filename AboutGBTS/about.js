0ee4543686ce1b150351ad0f0a35a6c644b336f5
ktlaguardia <97317434+ktlaguardia@users.noreply.github.com>
2023-04-03 02:36:20 +0000

    TODO: complete recipes.html

--- AboutGBTS/about.js
+++ AboutGBTS/about.js
@@ -2,16 +2,13 @@
 
 const titlecard = document.querySelector('#title');
 const colorText = document.getElementsByClassName('color-test');
-//titlecard.children[0].style.visibility = 'hidden'
 const bubbleTeaDesc = ['the delicious ', 'the colorful ', 'your favourite ', 'our homemade ', 'Generic ']
 let i = 0;
 console.log(colorText);
 
 setInterval(function(){
-    //titlecard.children[0].style.visibility == 'hidden' ? titlecard.children[0].style.visibility = 'visible' : titlecard.children[0].style.visibility = 'hidden';
     let prevColorText = colorText[i];
     prevColorText.classList.toggle('active');
     i==4 ? i = 4 : i++;
     colorText[i].classList.toggle('active');
-    //console.log(prevColorText, colorText[i], i)
-}, 3000);
+}, 2000);
